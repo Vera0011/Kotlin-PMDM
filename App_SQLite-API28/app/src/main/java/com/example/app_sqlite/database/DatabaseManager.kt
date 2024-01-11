4e5a87182da51500fd71cdae56b46369c751636f
Vera0011 <contact.vera0011@gmail.com>
2024-01-11 21:02:51 +0100

    Started testing integration

--- App_SQLite-API28/app/src/main/java/com/example/app_sqlite/database/DatabaseManager.kt
+++ App_SQLite-API28/app/src/main/java/com/example/app_sqlite/database/DatabaseManager.kt
@@ -1,6 +1,28 @@
 package com.example.app_sqlite.database
 
-class DatabaseManager
-{
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 
+class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "user_database.db", null, 1)
+{
+    @SuppressLint("Range")
+    override fun onCreate(db: SQLiteDatabase) {
+        val db = readableDatabase;
+        db.execSQL("CREATE TABLE Test(id INTEGER NOT NULL);");
+        db.execSQL("INSERT INTO Test VALUES(2)");
+        val cursor: Cursor = db.rawQuery("SELECT * FROM Test", null)
+
+        while (cursor.moveToNext()) {
+            println(cursor.getString(cursor.getColumnIndex("id")));
+        }
+
+        cursor.close();
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+    }
 }
