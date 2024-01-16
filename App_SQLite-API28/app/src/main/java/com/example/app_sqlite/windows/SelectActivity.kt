9f05b9dceecf45a43cff12dc14e2f60a83dd653f
Khrystyna <kpolish1dam1b@gmail.com>
2024-01-16 17:40:03 +0100

    Insert window updated

--- App_SQLite-API28/app/src/main/java/com/example/app_sqlite/windows/SelectActivity.kt
+++ App_SQLite-API28/app/src/main/java/com/example/app_sqlite/windows/SelectActivity.kt
@@ -1,8 +1,12 @@
 package com.example.app_sqlite.windows
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.app_sqlite.R
+
 
 class SelectActivity : AppCompatActivity()
 {
@@ -10,5 +14,13 @@ class SelectActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
+
+        /** val btnNavigate: Button = findViewById(R.id.insert)
+
+        btnNavigate.setOnClickListener(View.OnClickListener {
+            // Create an Intent to start SecondActivity
+            val intent = Intent(this@SelectActivity, InsertActivity::class.java)
+            startActivity(intent)
+        })*/
     }
 }
